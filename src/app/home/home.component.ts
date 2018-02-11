2dcd1ea2a38947e9694ecb7bc5fcc4e2b823bcbf
Cali93 <c.armut93@outlook.com>
2018-02-11 14:19:34 +0100

    animations on point

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { finalize } from 'rxjs/operators';
+
+import { QuoteService } from './quote.service';
+
+import { trigger, keyframes, animate, transition } from '@angular/animations';
+
+import * as kf from './keyframes';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('cardAnimator', [
+      transition('* => wobble', animate(100, keyframes(kf.wobble))),
+      transition('* => swing', animate(1000, keyframes(kf.swing))),
+      transition('* => jello', animate(1000, keyframes(kf.jello))),
+      transition('* => zoomOutRight', animate(1000, keyframes(kf.zoomOutRight))),
+      transition('* => slideOutLeft', animate(1000, keyframes(kf.slideOutLeft))),
+      transition('* => rotateOutUpRight', animate(1000, keyframes(kf.rotateOutUpRight))),
+      transition('* => flipOutY', animate(1000, keyframes(kf.flipOutY))),
+    ])
+  ]
+})
+export class HomeComponent implements OnInit {
+
+  animationState: string;
+  quote: string;
+  isLoading: boolean;
+
+  constructor(private quoteService: QuoteService) { }
+
+  ngOnInit() {
+    this.isLoading = true;
+    this.quoteService.getRandomQuote({ category: 'dev' })
+      .pipe(finalize(() => { this.isLoading = false; }))
+      .subscribe((quote: string) => { this.quote = quote; });
+  }
+
+  startAnimation(state) {
+    console.log(state)
+    if (!this.animationState){
+      this.animationState = state;
+    }
+  }
+
+  resetAnimationState() {
+    this.animationState = '';
+  }
+
+}
